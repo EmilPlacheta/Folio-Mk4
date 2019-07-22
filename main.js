83336455a949681b3e4e0b3ce92df11bf36d5546
Emil Placheta <emil.minoru@gmail.com>
2019-07-22 14:29:41 +0100

    added card items on 'my wprk page' + transitions

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const overlay = document.getElementById('overlay');
 const closeMenu = document.getElementById('close-menu');
 const openMenu = document.getElementById('open-menu');
+const cardContainer = document.querySelector(".card-container");
 
 
 //display overlay (nav) after clicking the hamburgr menu
 openMenu.addEventListener('click', function() {
   overlay.classList.add('show-menu');
+  cardContainer.classList.add('hide');
 });
 
 
