2309fd2bab9d0bb0f1d9e459abcf9a9140c50022
Emil <emil.minoru@gmail.com>
2020-05-26 15:22:53 +0100

    prevent function showing/hiding project descriptions from running on pages without project cards

--- main.js
+++ main.js
@@ -2,36 +2,54 @@
 const overlay = document.getElementById('overlay');
 const closeMenu = document.getElementById('close-menu');
 const openMenu = document.getElementById('open-menu');
-//const cardContainer = document.querySelector('.card-container');
+
 
 //my work page
 const cards = document.querySelectorAll('.card');
+const checForCard = document.querySelector('.card');
 const cardText = document.querySelectorAll('.card-text');
 
-//display project description paragraph when hovering over the card tile
-function handleHoverShow(e) {
-  let cardId = e.target.dataset.id;
+//display project description paragraph when hovering over the card tile, only on pages with cards.
 
-  cardText.forEach(p => {
-    if (p.dataset.id === cardId) {
-      p.classList.remove('hide');
+function showCardParagraphOnHover() {
+  if(!checForCard) {
+    return
+  } else {
+    function handleHoverShow(e) {
+      let cardId = e.target.dataset.id;
+      cardText.forEach(p => {
+        if (p.dataset.id === cardId) {
+          p.classList.remove('hide');
+        }
+      });
     }
-  });
+    
+    function handleHoverHide(e) {
+      let cardId = e.target.dataset.id;
+      cardText.forEach(p => {
+        if (p.dataset.id === cardId) {
+          p.classList.add('hide');
+        }
+      });
+    }
+
+    //show paragraph
+    cards.forEach(card => {
+      card.addEventListener('mouseenter', handleHoverShow);
+    });
+    //hide paragraph
+    cards.forEach(card => {
+      card.addEventListener('mouseleave', handleHoverHide);
+    });
+  }
 }
 
-function handleHoverHide(e) {
-  let cardId = e.target.dataset.id;
-  cardText.forEach(p => {
-    if (p.dataset.id === cardId) {
-      p.classList.add('hide');
-    }
-  });
-}
+showCardParagraphOnHover()
+
 
 //display overlay (nav) after clicking the hamburger menu
 openMenu.addEventListener('click', function() {
   overlay.classList.add('show-menu');
-  cardContainer.classList.add('hide');
 });
 
 //close overlay after clicking close btn
@@ -39,11 +57,4 @@ closeMenu.addEventListener('click', function() {
   overlay.classList.remove('show-menu');
 });
 
-//show paragraph
-cards.forEach(card => {
-  card.addEventListener('mouseenter', handleHoverShow);
-});
-//hide paragraph
-cards.forEach(card => {
-  card.addEventListener('mouseleave', handleHoverHide);
-});
+
